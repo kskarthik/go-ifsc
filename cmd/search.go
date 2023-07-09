099430c9d5b3dc7298097ee2a9e4a1f9ab918357
Sai Karthik <kskarthik@disroot.org>
2023-07-09 23:29:55 +0530

    feat(cli): format search results as ascii table

--- cmd/search.go
+++ cmd/search.go
@@ -6,11 +6,13 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 	"os"
 )
 
 var params SearchParams
+var output string
 
 // searchCmd represents the search command
 var searchCmd = &cobra.Command{
@@ -32,20 +34,58 @@ var searchCmd = &cobra.Command{
 			os.Exit(1)
 		}
 		if len(searchResults) > 0 {
-			for i := range searchResults {
-				PrintResult(searchResults[i])
-				fmt.Println("----------------------")
-			}
-			// display the result count after the last result
-			fmt.Println(len(searchResults), "results")
+			PrintTable(searchResults)
 		}
 	},
 }
 
+// print the result as cli table
+func PrintTable(v [][]string) {
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"IFSC", "BANK", "CITY", "STATE", "ADDRESS"})
+	// show result count in footer
+	if len(v) > 3 {
+		t.AppendFooter(table.Row{" ", " ", "", "", fmt.Sprintf("total: %v", len(v))})
+	}
+	for _, csv := range v {
+		row := table.Row{csv[1], csv[0], csv[10], csv[5], csv[6]}
+		t.AppendRow(row)
+	}
+	// config of columns
+	t.SetColumnConfigs([]table.ColumnConfig{
+		{
+			Name:     "ADDRESS",
+			WidthMax: 25,
+		},
+		{
+			Name:     "BANK",
+			WidthMax: 15,
+		},
+		{
+			Name:     "CITY",
+			WidthMax: 15,
+		},
+	})
+	// render the output format
+	switch output {
+	case "html":
+		t.RenderHTML()
+	case "csv":
+		t.RenderCSV()
+	case "md":
+		t.RenderMarkdown()
+	default:
+		// ascii
+		t.Render()
+	}
+}
 func init() {
 	rootCmd.AddCommand(searchCmd)
 	// flags
 	searchCmd.Flags().StringVarP(&params.match, "match", "m", DefaultMatch, SearchHelp)
 	// search count limit
 	searchCmd.Flags().IntVarP(&params.limit, "limit", "l", DefaultSearchLimit, "Limit the number of search results")
+	searchCmd.Flags().StringVarP(&output, "output", "o", "ascii", "specify output format, Available formats: html, md, csv")
 }
